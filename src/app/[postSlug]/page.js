ed361c9b1c0fc6142ea92ae9a203bb0edcaf1075
Lucas Harada <lucasyharada@gmail.com>
2024-12-10 18:06:04 -0300

    exercise 4

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -2,6 +2,7 @@ import BlogHero from "@/components/BlogHero";
 import { getBlogPostList, loadBlogPost } from "@/helpers/file-helpers";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import styles from "./postSlug.module.css";
+import CodeSnippet from "@/components/CodeSnippet";
 
 export async function generateStaticParams() {
   const blogPosts = await getBlogPostList();
@@ -34,7 +35,7 @@ async function BlogPost({ params }) {
         publishedOn={frontmatter.publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote source={content} components={{ pre: CodeSnippet }} />
       </div>
     </article>
   );
